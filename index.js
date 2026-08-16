c5a2be62acb343bb3f859c072b943fd2a6aab18f
ysaelopex <yasmimlopes0416@gmail.com>
2026-08-16 14:01:01 -0300

    Update index.js

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ import {
 } from 'discord.js';
 
 import fs from 'fs';
+import http from 'node:http';
 
 // ======================================================
 // BOT
@@ -4813,3 +4814,11 @@ if (
 
 }
 
+const PORT = process.env.PORT || 3000;
+
+http.createServer((req, res) => {
+    res.writeHead(200);
+    res.end('Bot online!');
+}).listen(PORT, '0.0.0.0', () => {
+    console.log(`Servidor web rodando na porta ${PORT}`);
+});
